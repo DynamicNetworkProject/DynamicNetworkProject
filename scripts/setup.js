f0f2e8db48ea0643b966457b991f1d7385902dd4
Martin <sirjordan1988@gmail.com>
2015-11-08 21:13:22 +0200

    Added setup js and some toher functionality

---
+++ scripts/setup.js
@@ -0,0 +1,18 @@
+var articleMaxLenght = 300;
+
+setArticlesLenght();
+
+function setArticlesLenght () {
+	var articles = document.getElementsByClassName('article');
+	for (var i = 0; i < articles.length; i++) {
+		var paragraph = articles[i].getElementsByTagName('p')[0];
+
+		if (paragraph.innerHTML.length >= articleMaxLenght) {
+			var text = paragraph.innerHTML.substring(0, articleMaxLenght);
+			text += '...';
+			paragraph.innerHTML = text;		
+		}else{
+			// TODO: Hide show/hide button
+		}
+	};
+}
