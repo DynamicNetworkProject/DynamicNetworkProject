a317c9ebbca525532b5a227f9651456b92a3a72b
gogo4ds <gogo4ds@gmail.com>
2015-11-21 20:32:30 +0200

    articles show hide

--- scripts/main-body.js
+++ scripts/main-body.js
@@ -2,13 +2,26 @@ function setArticlesLenght (articleMaxLenght) {
 	var articles = document.getElementsByClassName('article');
 	for (var i = 0; i < articles.length; i++) {
 		var paragraph = articles[i].getElementsByTagName('p')[0];
+		var articleArrows = articles[i].getElementsByClassName('show-hide')[0];
+		articleArrows.addEventListener('click', showHideArticle);
 
 		if (paragraph.innerHTML.length >= articleMaxLenght) {
-			var text = paragraph.innerHTML.substring(0, articleMaxLenght);
-			text += '...';
-			paragraph.innerHTML = text;		
+			var textPart = paragraph.innerHTML.substring(0, articleMaxLenght);
+			var hiddenPart = paragraph.innerHTML.substring(articleMaxLenght, articleMaxLenght.length);
+			textPart += '...';
+			paragraph.innerHTML = textPart;
+			var hiddenSpan = document.createElement('span');
+			hiddenSpan.className = 'hidden';
+			hiddenSpan.innerHTML = hiddenPart;
+			paragraph.appendChild(hiddenSpan);
 		}else{
 			// TODO: Hide show/hide button
 		}
 	};
+}
+
+function showHideArticle () {
+	var thisArticle = this.parentNode.parentNode;
+	var show = thisArticle.getElementsByClassName('hidden')[0].className = 'showed';
+	// thisArticle.getElementsByClassName('showed')[0].className = 'hidden';
 }
