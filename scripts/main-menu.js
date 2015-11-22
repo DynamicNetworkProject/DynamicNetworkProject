e913982efa3ea27cfbdcb47162d9ef21dba41257
Martin <sirjordan1988@gmail.com>
2015-11-22 09:34:31 +0200

    Update Velislav's work

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,6 +1,22 @@
-function mainMenuScript (containerId){
-    var container = document.querySelector(containerId);
-
+function setMainMenuEvents () {
+    var element = document.getElementById('menu-arrow')
+    element.addEventListener("click", showHideMainMenu);
 }
 
-mainMenuScript('#header-container');
+function showHideMainMenu (argument) {
+    var button = document.getElementById('menu-arrow');
+    var wrapper = document.getElementById('header-container');
+    var elements = wrapper.getElementsByTagName('li');
+    for (var i = elements.length - 1; i >= 0; i--) {
+        if (elements[i].className !== 'mainNavigation' ){
+         elements[i].className = 'hidden';
+         if (button.className == 'open') {
+             button.className = 'closedd'
+             elements[i].className = 'hidden';
+         }else {
+             elements[i].className = 'show'
+             button.className = 'open'
+         }
+    }
+
+};
