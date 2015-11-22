9265f6d1f852a9eb258be2ac051c981abd9c9f6e
Martin <sirjordan1988@gmail.com>
2015-11-22 09:44:09 +0200

    Small changes

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -9,14 +9,15 @@ function showHideMainMenu (argument) {
     var elements = wrapper.getElementsByTagName('li');
     for (var i = elements.length - 1; i >= 0; i--) {
         if (elements[i].className !== 'mainNavigation' ){
-         elements[i].className = 'hidden';
-         if (button.className == 'open') {
+            elements[i].className = 'hidden';
+            if (button.className == 'open') {
              button.className = 'closedd'
              elements[i].className = 'hidden';
-         }else {
+            }else {
              elements[i].className = 'show'
              button.className = 'open'
-         }
-    }
+            }  
+        }
 
-};
+    };
+}
