c3b21546931db3261ce54971ec2c1a15bbfb320d
velislavskiqt <velislavskiqt@gmail.com>
2015-11-29 19:45:30 +0200

    Main-menu

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -7,17 +7,22 @@ function showHideMainMenu (argument) {
     var button = document.getElementById('menu-arrow');
     var wrapper = document.getElementById('header-container');
     var elements = wrapper.getElementsByTagName('li');
+
     for (var i = elements.length - 1; i >= 0; i--) {
         if (elements[i].className !== 'mainNavigation' ){
             elements[i].className = 'hidden';
-            if (button.className == 'open') {
-             button.className = 'closedd'
-             elements[i].className = 'hidden';
-            }else {
-             elements[i].className = 'show'
-             button.className = 'open'
-            }  
-        }
 
+            if (button.className != 'open') {
+                elements[i].className = 'hidden';
+            }else {
+                elements[i].className = 'show'
+            }
+        }
     };
+
+    if (button.className == 'closed') {
+         button.className = 'open';
+    }else {
+         button.className = 'closed'
+    } 
 }
