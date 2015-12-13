284d1318d3f0b1d3e090058772c1362953a3502a
velislavskiqt <velislavskiqt@gmail.com>
2015-12-13 19:44:05 +0200

    small re-write
    main-menu using jQuery

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,29 +1,11 @@
 function setMainMenuEvents () {
-    var element = document.getElementById('menu-arrow')
-    element.addEventListener("click", showHideMainMenu);
-}
+
+    $('#menu-arrow').on('click', showHideMainMenu);
+};
 
 function showHideMainMenu (argument) {
-    var button = document.getElementById('menu-arrow');
-    var wrapper = document.getElementById('header-container');
-    var elements = wrapper.getElementsByTagName('li');
-
-    for (var i = elements.length - 1; i >= 0; i--) {
-        if (elements[i].className !== 'mainNavigation' ){
-            elements[i].className = 'hidden';
-
-            if (button.className != 'open') {
-                elements[i].className = 'hidden';
-            }else {
-                elements[i].className = 'show'
-            }
-        }
-    };
-
-    if (button.className == 'closed') {
-         button.className = 'open';
-    }else {
-         button.className = 'closed'
-    } 
-}
+    $mainNavs = $('#header-container li.mainNavigation');
+    $('#header-container li').not($mainNavs).toggle(300);
+    $('#arrow-pic').toggleClass('flipped-x', '');
+};
 
