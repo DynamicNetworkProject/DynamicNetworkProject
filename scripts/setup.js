0b167bca415bca85c56f8eabf0e6669b3c9ec5a6
georgi <gogo4ds@gmail.com>
2015-12-20 19:33:33 +0200

    rendering articles

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 $(document).ready(function () {  
     $("#main-container").load("partial-views/articles-partial.html", setMainBodyEvents);
 	
-=======
-$(document).ready(function () {
-	$("#main-container").load("partial-views/articles-partial.html", setMainBodyEvents);
-	//setMainBodyEvents();
->>>>>>> origin/master
     $("#friends-list-wrapper").load("partial-views/friends-list-partial.html", setFriendsEvents);
 	
     setMainMenuEvents();
