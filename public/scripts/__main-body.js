cb7b8f910f97325ccb6f21d2ba31d44ba4195e57
Georgi <gogo4ds@gmail.com>
2016-01-10 20:29:26 +0200

    getJSON in articles

---
+++ public/scripts/__main-body.js
@@ -0,0 +1,72 @@
+function setupMainBody() {
+    processData();
+    var articleMaxLenght = 300;
+    
+    //renderArticles(processData);
+    setArticlesLenght(articleMaxLenght);
+    closeArticle();
+};
+
+function setArticlesLenght(articleMaxLenght) {
+    var articles = document.getElementsByClassName('article');
+    for (var i = 0; i < articles.length; i++) {
+        var paragraph = articles[i].getElementsByTagName('p')[0];
+        var articleArrows = articles[i].getElementsByClassName('show-hide')[0];
+        articleArrows.addEventListener('click', showHideArticle);
+
+        if (paragraph.innerHTML.length >= articleMaxLenght) {
+            var textPart = paragraph.innerHTML.substring(0, articleMaxLenght);
+            var hiddenPart = paragraph.innerHTML.substring(articleMaxLenght, articleMaxLenght.length);
+            var spanWithDots = document.createElement('span');
+            spanWithDots.className = 'spanWithDots';
+            spanWithDots.innerHTML = '...';
+            paragraph.innerHTML = textPart;
+            paragraph.appendChild(spanWithDots);
+            var hiddenSpan = document.createElement('span');
+            hiddenSpan.className = 'hidden';
+            hiddenSpan.innerHTML = hiddenPart;
+            paragraph.appendChild(hiddenSpan);
+        }
+    };
+}
+
+function processData() {
+    $.getJSON('http://localhost:8081/api/articles', renderArticles);
+}
+
+function closeArticle() {
+    var closeBtn = document.getElementsByClassName("close-article-btn");
+    for (var i = 0; i < closeBtn.length; i++) {
+        closeBtn[i].addEventListener('click', closeArticleWindow);
+    }
+};
+
+function showHideArticle() {
+    var thisArticle = this.parentNode.parentNode;
+    var hidden = thisArticle.getElementsByClassName('hidden')[0];
+    var showed = thisArticle.getElementsByClassName('showed')[0];
+    var arrow = thisArticle.getElementsByClassName('show-hide')[0];
+    if (hidden.className === 'hidden') {
+        thisArticle.getElementsByClassName('hidden')[0].className = 'showed';
+        thisArticle.getElementsByClassName('spanWithDots')[0].classList.add('hidden');
+        arrow.classList.add('flipped');
+    } else {
+        thisArticle.getElementsByClassName('showed')[0].className = 'hidden';
+        thisArticle.getElementsByClassName('spanWithDots')[0].classList.remove('hidden');
+        arrow.classList.remove('flipped');
+        // TODO: flipArrowBug;
+    }
+};
+
+//Handlebars
+function renderArticles(data) {
+    // var context = new DataContext();
+    // console.log(typeof data);
+    var articlesData = $.parseJSON(data);
+    var htmlTemplate = document.getElementById("articles-template").innerHTML;
+    var template = Handlebars.compile(htmlTemplate);
+
+    var objectsForRendering = { articles: articlesData };
+    var generatedHtml = template(objectsForRendering);
+    document.getElementById("main-container").innerHTML = generatedHtml;
+};
